efc4ce24a584b7613170a7cc3a19cbd8a60fce56
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-05-21 21:14:21 -0700

    js commit Cypress project

---
+++ cypress/e2e/formspec.cy.js
@@ -0,0 +1,23 @@
+describe("Submits a rating", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+  });
+  it("Creates a booking", () => {
+    cy.get('[data-cy="input-name"]').type('Pizza House');
+    cy.get('[data-cy="input-address"]').type('123r PL NE, Seattle, USA');
+    cy.get('[data-cy="input-phone"]').type('111-123-1234');
+    cy.get('[data-cy="input-cuisine"]').type('Italian');
+    cy.get('[data-cy="input-rating"]').type('5');
+
+    //Submit the form
+    cy.get('[data-cy="rating-form"]').submit();
+
+    //Assert new restaurant appears with correct info
+    cy.get('[data-cy="Pizza House"] ul').within(() => {
+      cy.contains('li', '123r PL NE, Seattle, USA').should('be.visible');
+      cy.contains('li', '111-123-1234').should('be.visible');
+      cy.contains('li', 'Italian').should('be.visible');
+      cy.contains('li', '5').should('be.visible');
+    });
+  });
+});
--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import cypress from 'eslint-plugin-cypress'
 
 export default [
   { ignores: ['dist'] },
@@ -28,6 +29,24 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+
+  {
+    files: ['cypress/**/*.{js,jsx}'], // Only apply Cypress settings to Cypress test files
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha, // needed for `describe`, `it`, `beforeEach`, etc.
+        cy: 'readonly',    // Cypress itself
+      },
+    },
+    plugins: {
+      cypress,
+    },
+    rules: {
+      ...cypress.configs.recommended.rules,
     },
   },
 ]
